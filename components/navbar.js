346b1d42460321fb909c10fabb4344e8f69a3c67
roxy31512 <98624993+roxy31512@users.noreply.github.com>
2026-08-19 10:21:02 -0500

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -273,7 +273,7 @@ class CustomNavbar extends HTMLElement {
             <a href="/cv">CV</a>
             <a href="/blog">Blog</a>
             <a href="/mentorship">Mentorship</a>
-            <a href="/tutoring">Tutoring</a>
+            <a href="/editorial-consulting">Editorial Consulting</a>
             <a href="/contact" class="cta-link">Contact</a>
           </nav>
 
